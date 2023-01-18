622de7930c92ea437630683dd4de56b39b366726
Mariano Daniel Gobea Alcoba <gobeamariano@gmail.com>
2023-01-18 11:00:01 -0300

    first commit 18.1.2023

--- src/templates/Template.js
+++ src/templates/Template.js
@@ -1,4 +1,7 @@
-import getData from '../utils/getData.js';
+import getData from '@utils/getData.js';
+import github from '@images/github.png';
+import twitter from '@images/twitter.png';
+import linkedin from '@images/R.png';
 
 const Template = async () => {
   const data = await getData();
@@ -23,14 +26,14 @@ const Template = async () => {
           </ul>
         </div>
         <div class="card_social">
-          <a href="https://twitter.com/gndx">
-            <img src="../src/assets/images/twitter.png" />
+          <a href="https://twitter.com/Mgobeaalcoba">
+            <img src="${twitter}" />
           </a>
-          <a href="https://github.com/gndx">
-            <img src="../src/assets/images/github.png" />
+          <a href="https://github.com/Mgobeaalcoba">
+            <img src="${github}" />
           </a>
-          <a href="https://instagram.com/gndx">
-            <img src="../src/assets/images/instagram.png" />
+          <a href="https://www.linkedin.com/in/mariano-gobea-alcoba/">
+            <img src="${linkedin}" />
           </a>
         </div>
       </div>

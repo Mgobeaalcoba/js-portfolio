607d17a06794ca3ce9d1699b411b1fa6d27741fd
Mariano Daniel Gobea Alcoba <gobeamariano@gmail.com>
2023-01-20 10:13:42 -0300

    first commit 20.1.2023 without deploy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import Template from '@templates/Template.js';
 import '@styles/main.css';
 import '@styles/vars.styl'
+console.log('Hola Mundo');
+console.log('Chau Mundo');
+console.log('Esto es una pruba de compilación en tiempo real');
 
 (async function App() {
   const main = null || document.getElementById('main');
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,10 +4,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -16,7 +13,8 @@ module.exports = {
         filename: '[name].[contenthash].js',
         assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
-    mode: 'production',
+    mode: 'development',
+    //watch: false,
     resolve: {
         extensions: ['.js'],
         alias: {
@@ -80,13 +78,5 @@ module.exports = {
             ]
         }),
         new Dotenv(),
-        new CleanWebpackPlugin(),
-    ],
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin(),
-            new TerserPlugin(),
-        ]
-    }
+    ]
 }
